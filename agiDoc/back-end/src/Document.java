c469a25eae52b200f944e53c51e8ff393c84db8f
Mariana Nascimento <mariana.sn.silva@gmail.com>
2024-01-03 20:40:42 -0300

    refact: modify class name

---
+++ agiDoc/back-end/src/Document.java
@@ -0,0 +1,28 @@
+public class Document {
+    private String protocol;
+    private String content;
+
+    public Document() {
+    }
+
+    public Document(String protocol, String content) {
+        this.protocol = protocol;
+        this.content = content;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
