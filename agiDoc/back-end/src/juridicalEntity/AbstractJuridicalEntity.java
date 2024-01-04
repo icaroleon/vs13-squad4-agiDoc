a10c04eb94d61bd6c148ce7060d5a5276c4532fa
Aron Adams <aronrapetto@hotmail.com>
2024-01-04 02:12:50 -0300

    feat: create AbstractJuridicalEntity class

---
+++ agiDoc/back-end/src/juridicalEntity/AbstractJuridicalEntity.java
@@ -0,0 +1,45 @@
+package juridicalEntity;
+
+import document.Document;
+
+public abstract class AbstractJuridicalEntity implements IJuridicalEntity {
+    private String cnpj;
+    private String endereco;
+    private String contato;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    @Override
+    public boolean signDocument(Document document) {
+        System.out.println("Documento assinado!");
+        return true;
+    }
+
+    @Override
+    public boolean sendSignedDocument(Process process) {
+        System.out.println("Documento enviado com sucesso!");
+        return false;
+    }
+}
