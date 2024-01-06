2b873821b7cd7ccb33211f971d5a79e1f91faf71
Ícaro Leon <100085859+icaroleon@users.noreply.github.com>
2024-01-06 18:55:42 -0300

    edit: delete comments

--- agiDoc/back-end/src/process/ProcessController.java
+++ agiDoc/back-end/src/process/ProcessController.java
@@ -2,7 +2,7 @@ package process;
 
 import java.util.ArrayList;
 
- /*public class ProcessController {
+ public class ProcessController {
     private static ArrayList<Process> processes = new ArrayList<>();
 
     public ProcessController() {
@@ -53,4 +53,4 @@ import java.util.ArrayList;
             }
         }
     }
-}*/
+}
