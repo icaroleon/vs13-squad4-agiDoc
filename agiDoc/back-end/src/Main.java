758afc7a359fd9f4e209101211b81cd3b7c6bc97
Rodrigo Carvalho <rodrigofcc.dev@gmail.com>
2024-01-07 14:48:40 -0300

    add: new CompetitorController class and branch

--- agiDoc/back-end/src/Main.java
+++ agiDoc/back-end/src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         Stack<String> history = new Stack<>();
         boolean isNotValidLogin = true;
