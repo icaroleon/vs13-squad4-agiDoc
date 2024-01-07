ebcf9f7cb110333ba29addd781f93d0882a2845f
Aron Adams <aronrapetto@hotmail.com>
2024-01-07 15:16:57 -0300

    refactor: seed instances

--- agiDoc/back-end/src/data/Data.java
+++ agiDoc/back-end/src/data/Data.java
@@ -1,53 +1,54 @@
 package data;
 
 import entities.document.Document;
+import entities.document.DocumentType;
 import entities.employee.Employee;
 import entities.competitor.Competitor;
 import entities.institution.Institution;
 import entities.process.Process;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Data {
+public abstract class Data {
     public static Institution institution;
 
     public static void seed() {
 //      instâncias de documentos para Processos
-        Document documentOneProcessOne = new Document("1", "2024-01-01", "documento: Contrato");
-        Document documentTwoProcessOne = new Document("2", "2024-01-02", "documento: Edital");
-        Document documentThreeProcessOne = new Document("3", "2024-01-03", "documento: Parecer Jurídico");
+        Document documentOneProcessOne = new Document("1", LocalDate.of(2024, 1, 1), "Instituição", "id:1", DocumentType.ATTACHMENTS, "documento: Contrato");
+        Document documentTwoProcessOne = new Document("2", LocalDate.of(2024, 1, 2), "Instituição", "id:2", DocumentType.ATTACHMENTS, "documento: Edital");
+        Document documentThreeProcessOne = new Document("3", LocalDate.of(2024, 1, 3), "Instituição", "id:3", DocumentType.ATTACHMENTS, "documento: Parecer Jurídico");
 
-        Document documentOneProcessTwo = new Document("4", "2024-01-04", "documento: Contrato");
-        Document documentTwoProcessTwo = new Document("5", "2024-01-05", "documento: Delegação de Competência");
-        Document documentThreeProcessTwo = new Document("6", "2024-01-06", "documento: Termo de Referência");
+        Document documentOneProcessTwo = new Document("4", LocalDate.of(2024, 1, 5), "Instituição", "id:4", DocumentType.ATTACHMENTS, "documento: Contrato");
+        Document documentTwoProcessTwo = new Document("5", LocalDate.of(2024, 1, 6), "Instituição", "id:5", DocumentType.ATTACHMENTS, "documento: Delegação de Competência");
+        Document documentThreeProcessTwo = new Document("6", LocalDate.of(2024, 1, 7), "Instituição", "id:6", DocumentType.ATTACHMENTS, "documento: Termo de Referência");
 
-        Document documentOneProcessThree = new Document("7", "2024-01-07", "documento: Contrato");
-        Document documentTwoProcessThree = new Document("8", "2024-01-08", "documento: Manifestação");
-        Document documentThreeProcessThree = new Document("9", "2024-01-09", "documento: Justificativa de Necessidade");
+        Document documentOneProcessThree = new Document("7", LocalDate.of(2024, 1, 8), "Instituição", "id:7", DocumentType.ATTACHMENTS, "documento: Contrato");
+        Document documentTwoProcessThree = new Document("8", LocalDate.of(2024, 1, 9), "Instituição", "id:8", DocumentType.ATTACHMENTS, "documento: Manifestação");
+        Document documentThreeProcessThree = new Document("9", LocalDate.of(2024, 1, 10), "Instituição", "id:9", DocumentType.ATTACHMENTS, "documento: Justificativa de Necessidade");
 
 //      instâncias de documentos para Concorrentes
-        Document documentOneCompetitorOne = new Document("10", "2024-01-10","documento: Certidão");
-        Document documentTwoCompetitorOne = new Document("11", "2024-01-11","documento: Termo de Referência");
+        Document documentOneCompetitorOne = new Document("10", LocalDate.of(2024, 1, 11), "Concorrente", "id:10", DocumentType.ATTACHMENTS, "documento: Certidão");
+        Document documentTwoCompetitorOne = new Document("11", LocalDate.of(2024, 1, 12), "Concorrente", "id:12", DocumentType.ATTACHMENTS, "documento: Termo de Referência");
 
-        Document documentOneCompetitorTwo = new Document("12", "2024-01-12","documento: Certidão");
-        Document documentTwoCompetitorTwo = new Document("13", "2024-01-13","documento: Portaria");
+        Document documentOneCompetitorTwo = new Document("12", LocalDate.of(2024, 1, 13), "Concorrente", "id:13", DocumentType.ATTACHMENTS, "documento: Certidão");
+        Document documentTwoCompetitorTwo = new Document("13", LocalDate.of(2024, 1, 14), "Concorrente", "id:14", DocumentType.ATTACHMENTS, "documento: Portaria");
 
-        Document documentOneCompetitorThree = new Document("14", "2024-01-14","documento: Certidão");
-        Document documentTwoCompetitorThree = new Document("15", "2024-01-15","documento: Anexos");
+        Document documentOneCompetitorThree = new Document("14", LocalDate.of(2024, 1, 15), "Concorrente", "id:15", DocumentType.ATTACHMENTS, "documento: Certidão");
+        Document documentTwoCompetitorThree = new Document("15", LocalDate.of(2024, 1, 16), "Concorrente", "id:16", DocumentType.ATTACHMENTS, "documento: Anexos");
 
-        Document documentOneCompetitorFour = new Document("16", "2024-01-16","documento: Certidão");
-        Document documentTwoCompetitorFour = new Document("17", "2024-01-17","documento: Justificativa de Necessidade");
+        Document documentOneCompetitorFour = new Document("16", LocalDate.of(2024, 1, 17), "Concorrente", "id:17", DocumentType.ATTACHMENTS, "documento: Certidão");
+        Document documentTwoCompetitorFour = new Document("17", LocalDate.of(2024, 1, 18), "Concorrente", "id:18", DocumentType.ATTACHMENTS, "documento: Justificativa de Necessidade");
 
-        Document documentOneCompetitorFive = new Document("18", "2024-01-18","documento: Certidão");
-        Document documentTwoCompetitorFive = new Document("19", "2024-01-19","documento: Delegação de Competência");
+        Document documentOneCompetitorFive = new Document("18", LocalDate.of(2024, 1, 19), "Concorrente", "id:19", DocumentType.ATTACHMENTS, "documento: Certidão");
+        Document documentTwoCompetitorFive = new Document("19", LocalDate.of(2024, 1, 20), "Concorrente", "id:10", DocumentType.ATTACHMENTS, "documento: Delegação de Competência");
 
-        Document documentOneCompetitorSix = new Document("20", "2024-01-20","documento: Certidão");
-        Document documentTwoCompetitorSix = new Document("21", "2024-01-21","documento: Portaria");
+        Document documentOneCompetitorSix = new Document("20", LocalDate.of(2024, 1, 21), "Concorrente", "id:21", DocumentType.ATTACHMENTS, "documento: Certidão");
+        Document documentTwoCompetitorSix = new Document("21", LocalDate.of(2024, 1, 22), "Concorrente", "id:22", DocumentType.ATTACHMENTS, "documento: Portaria");
 
 //      instâncias de Funcionários para Concorrentes
-//      TODO: Setar company
         Employee employeeCompetitorOne = new Employee("João", "joao_girassol", "4321", "Rua dos Girassóis", "9 0000-0000");
         Employee employeeCompetitorTwo = new Employee("Maria", "maria_rosa", "1234", "Rua das rosa", "9 0000-0000");
         Employee employeeCompetitorThree = new Employee("Everton", "everton_bromelia", "1234", "Rua das Bromélias", "9 0000-0000");
@@ -56,6 +57,7 @@ public class Data {
         Employee employeeCompetitorSix = new Employee("Robson", "Robson_dos_anjos", "1234", "Rua celeste", "9 0000-0000");
 
 //      instâncias de Funcionários para Instituição
+//      TODO: Setar company
         Employee employeeOneInstitution = new Employee("Aron", "aron_usuario", "aron_senha", "aron_endereco", "9 0000-0000");
         Employee employeeTwoInstitution = new Employee("Gabriel", "gabriel_usuario", "gabriel_senha", "gabriel_endereco", "9 1111-1111");
         Employee employeeThreeInstitution = new Employee("Mari", "mari_usuario", "mari_senha", "mari_endereco", "9 2222-2222");
@@ -71,83 +73,88 @@ public class Data {
                 "00.0.000/0001-00",
                 "Rua dos bobos",
                 "9 0000-0000",
-                "InnovateTech Solutions",
-                new ArrayList<Document>(Arrays.asList(
-                        documentOneCompetitorOne,
-                        documentTwoCompetitorOne
-                )),
-                new ArrayList<>(List.of(employeeCompetitorOne))
+                "InnovateTech Solutions"
         );
+        competitorOneProcessOne.setDocuments(new ArrayList<Document>(Arrays.asList(
+                documentOneCompetitorOne,
+                documentTwoCompetitorOne
+        )));
+        competitorOneProcessOne.setEmployees(new ArrayList<>(List.of(employeeCompetitorOne)));
+
         employeeCompetitorOne.setCompany(competitorOneProcessOne);
 
         Competitor competitorTwoProcessOne = new Competitor(
                 "11.1.111/1111-11",
                 "Rua dos loucos",
                 "9 0000-0000",
-                "Quantum Dynamics Innovations",
-                new ArrayList<Document>(Arrays.asList(
-                        documentOneCompetitorTwo,
-                        documentTwoCompetitorTwo
-                )),
-                new ArrayList<>(List.of(employeeCompetitorTwo))
+                "Quantum Dynamics Innovations"
         );
+        competitorTwoProcessOne.setDocuments(new ArrayList<Document>(Arrays.asList(
+                documentOneCompetitorTwo,
+                documentTwoCompetitorTwo
+        )));
+        competitorTwoProcessOne.setEmployees(new ArrayList<>(List.of(employeeCompetitorTwo)));
+
         employeeCompetitorTwo.setCompany(competitorTwoProcessOne);
 
         Competitor competitorOneProcessTwo = new Competitor(
                 "22.2.222/2221-22",
                 "Rua dos azedos",
                 "9 0000-0000",
-                "Nexus Innovations Group",
-                new ArrayList<Document>(Arrays.asList(
-                        documentOneCompetitorThree,
-                        documentTwoCompetitorThree
-                )),
-                new ArrayList<>(List.of(employeeCompetitorThree))
+                "Nexus Innovations Group"
         );
+        competitorOneProcessTwo.setDocuments(new ArrayList<Document>(Arrays.asList(
+                documentOneCompetitorThree,
+                documentTwoCompetitorThree
+        )));
+        competitorOneProcessTwo.setEmployees(new ArrayList<>(List.of(employeeCompetitorThree)));
+
         employeeCompetitorThree.setCompany(competitorOneProcessTwo);
 
         Competitor competitorTwoProcessTwo = new Competitor(
                 "33.3.333/3331-33",
                 "Rua dos desavisados",
                 "9 0000-0000",
-                "Stratosphere Solutions",
-                new ArrayList<Document>(Arrays.asList(
-                        documentOneCompetitorFour,
-                        documentTwoCompetitorFour
-                )),
-                new ArrayList<>(List.of(employeeCompetitorFour))
+                "Stratosphere Solutions"
         );
+        competitorTwoProcessTwo.setDocuments(new ArrayList<Document>(Arrays.asList(
+                documentOneCompetitorFour,
+                documentTwoCompetitorFour
+        )));
+        competitorTwoProcessTwo.setEmployees(new ArrayList<>(List.of(employeeCompetitorFour)));
+
         employeeCompetitorFour.setCompany(competitorTwoProcessTwo);
 
         Competitor competitorOneProcessThree = new Competitor(
                 "44.4.444/4441-44",
                 "Rua dos bem aventurados",
                 "9 0000-0000",
-                "Zenith Enterprises",
-                new ArrayList<Document>(Arrays.asList(
-                        documentOneCompetitorFive,
-                        documentTwoCompetitorFive
-                )),
-                new ArrayList<>(List.of(employeeCompetitorFive))
+                "Zenith Enterprises"
         );
+        competitorOneProcessThree.setDocuments(new ArrayList<Document>(Arrays.asList(
+                documentOneCompetitorFive,
+                documentTwoCompetitorFive
+        )));
+        competitorOneProcessThree.setEmployees(new ArrayList<>(List.of(employeeCompetitorFive)));
+
         employeeCompetitorFive.setCompany(competitorOneProcessThree);
 
         Competitor competitorTwoProcessThree = new Competitor(
                 "55.5.555/5551-55",
                 "Rua dos moradores",
                 "9 0000-0000",
-                "Nebula Innovations Inc.",
-                new ArrayList<Document>(Arrays.asList(
-                        documentOneCompetitorSix,
-                        documentTwoCompetitorSix
-                )),
-                new ArrayList<>(List.of(employeeCompetitorSix))
+                "Nebula Innovations Inc."
         );
+        competitorTwoProcessThree.setDocuments(new ArrayList<Document>(Arrays.asList(
+                documentOneCompetitorSix,
+                documentTwoCompetitorSix
+        )));
+        competitorTwoProcessThree.setEmployees(new ArrayList<>(List.of(employeeCompetitorSix)));
+
         employeeCompetitorSix.setCompany(competitorTwoProcessThree);
 
 //      instâncias de Processos
         Process processOne = new Process(
-                "111111",
                 "Pregão Eletrônico",
                 "pregrão eletrônico descrição",
                 new ArrayList<Competitor>(Arrays.asList(competitorOneProcessOne, competitorTwoProcessOne)),
@@ -159,7 +166,6 @@ public class Data {
         );
 
         Process processTwo = new Process(
-                "222222",
                 "Tomada de Preços",
                 "tomada de preços descrição",
                 new ArrayList<Competitor>(Arrays.asList(competitorOneProcessTwo, competitorTwoProcessTwo)),
@@ -171,7 +177,6 @@ public class Data {
         );
 
         Process processThree = new Process(
-                "33333",
                 "Licitação Pública para Contratação de Obra",
                 "licitação pública para contratação de obra descrição",
                 new ArrayList<Competitor>(Arrays.asList(competitorOneProcessThree, competitorTwoProcessThree)),
@@ -183,6 +188,14 @@ public class Data {
         );
 
 //      TODO: Setar processo em concorrentes
+        competitorOneProcessOne.setProcess(processOne);
+        competitorTwoProcessOne.setProcess(processOne);
+
+        competitorOneProcessTwo.setProcess(processTwo);
+        competitorTwoProcessTwo.setProcess(processTwo);
+
+        competitorOneProcessThree.setProcess(processThree);
+        competitorTwoProcessThree.setProcess(processThree);
 
 //      Retorna instância de Institution
         institution = new Institution(
