ba823a95dd79da4163774856438d342829c4b11c
Rodrigo Carvalho <rodrigofcc.dev@gmail.com>
2024-01-06 22:57:56 -0300

    fix: competitor entitie, AbstractJuridical atributes and constructor, Employee constructor

--- agiDoc/back-end/src/entities/competitor/Competitor.java
+++ agiDoc/back-end/src/entities/competitor/Competitor.java
@@ -1,25 +1,56 @@
 package entities.competitor;
 
 import entities.document.Document;
+import entities.employee.Employee;
+import entities.juridical.AbstractJuridical;
 
 import java.util.ArrayList;
 
-public class Competitor {
+public class Competitor extends AbstractJuridical {
 
-    private boolean competitor;
+    private String companyName;
+    private boolean isContracted = false;
+    private Process process;
     private ArrayList<Document> documents;
 
-    public Competitor(boolean competitor, ArrayList<Document> documents) {
-        this.competitor = competitor;
+
+    public Competitor(){
+    }
+
+    public Competitor(String cpnj, String address, String contact, String companyName) {
+        super(cpnj, address, contact);
+        this.companyName = companyName;
+    }
+
+    public Competitor(String cpnj, String address, String contact, ArrayList<Employee> employees, String companyName, Process process, ArrayList<Document> documents) {
+        super(cpnj, address, contact, employees);
+        this.companyName = companyName;
+        this.process = process;
         this.documents = documents;
     }
 
-    public boolean isCompetitor() {
-        return competitor;
+    public String getCompanyName() {
+        return companyName;
     }
 
-    public void setCompetitor(boolean competitor) {
-        this.competitor = competitor;
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public boolean isContracted() {
+        return isContracted;
+    }
+
+    public void setContracted(boolean contracted) {
+        isContracted = contracted;
+    }
+
+    public Process getProcess() {
+        return process;
+    }
+
+    public void setProcess(Process process) {
+        this.process = process;
     }
 
     public ArrayList<Document> getDocuments() {
