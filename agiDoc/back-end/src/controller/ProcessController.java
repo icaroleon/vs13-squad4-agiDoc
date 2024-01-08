4387f8f42a770d624cdfc4839d1fb9b81e7569d2
ViniPiresB <viniciuspbarreto1@gmail.com>
2024-01-08 00:35:16 -0300

    ✨ feat: chooseCompetitor and closeProcess

--- agiDoc/back-end/src/controller/ProcessController.java
+++ agiDoc/back-end/src/controller/ProcessController.java
@@ -1,14 +1,18 @@
 package controller;
 
 import data.Data;
+import entities.competitor.Competitor;
 import entities.process.Process;
+import service.CompetitorService;
 import service.ProcessService;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public abstract class ProcessController {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final ProcessService service = new ProcessService(Data.institution.getProcesses());
+    private static final ProcessService processService = new ProcessService(Data.institution.getProcesses());
+    private static final CompetitorService competitorService = new CompetitorService();
 
     public static void createProcess() {
         System.out.println("Digite o titulo do processo: ");
@@ -19,8 +23,8 @@ public abstract class ProcessController {
 
         Process process = new Process(title, description);
 
-        service.create(process);
-        Data.institution.setProcesses(service.getAll());
+        processService.create(process);
+        Data.institution.setProcesses(processService.getAll());
 
         System.out.println("Processo criado");
         System.out.println(process);
@@ -29,7 +33,7 @@ public abstract class ProcessController {
     public static Process get(String id) {
         Process process = null;
         try {
-            process = service.get(id);
+            process = processService.get(id);
             System.out.println(process.toString());
         } catch (Exception e) {
             System.out.println("Processo nao encontrado!");
@@ -38,7 +42,7 @@ public abstract class ProcessController {
     }
 
     public static void getAll() {
-        for (Process process : service.getAll()) {
+        for (Process process : processService.getAll()) {
             System.out.println(process.toString());
         }
     }
@@ -53,8 +57,8 @@ public abstract class ProcessController {
         Process newProcess = new Process(title, description);
 
         try {
-            service.update(processId, newProcess);
-            Data.institution.setProcesses(service.getAll());
+            processService.update(processId, newProcess);
+            Data.institution.setProcesses(processService.getAll());
 
             System.out.println("Processo " + processId + "atualizado com sucesso.");
             System.out.println(newProcess);
@@ -65,8 +69,8 @@ public abstract class ProcessController {
 
     public static void delete(String processId) {
         try {
-            service.delete(processId);
-            Data.institution.setProcesses(service.getAll());
+            processService.delete(processId);
+            Data.institution.setProcesses(processService.getAll());
 
             System.out.println("Processo n°: " + processId + " excluído com sucesso!");
         } catch (Exception e) {
@@ -76,18 +80,42 @@ public abstract class ProcessController {
 
     public static void closeProcess(String processId, Process process) {
         try {
-//          TODO: Validar se processo tem concorrente escolhido.
+            Process actualProcess = processService.get(processId);
+
+            if (actualProcess.getContracted() == null) {
+                System.out.println("ERRO: Processo não tem concorrente contratado!");
+                return;
+            }
+
             process.setStatus("Fechado");
 
-            service.update(processId, process);
-            Data.institution.setProcesses(service.getAll());
+            processService.update(processId, process);
+            Data.institution.setProcesses(processService.getAll());
 
             System.out.println("Processo n°: " + processId + " fechado.");
         } catch (Exception e) {
             System.err.println("Erro: " + e.getMessage());
         }
     }
-    // TODO: criar método para escolher concorrente no processo
-    public static void chooseCompetitor(String processId, Process process) {
+
+    public static void chooseCompetitor(String processId) {
+        try {
+            Process process = processService.get(processId);
+            ArrayList<Competitor> competitors = process.getCompetitors();
+
+            competitors.forEach(competitor -> System.out.println(competitor.toString()));
+
+            System.out.println("Selecione o id concorrente a ser escolhido: ");
+            String competitorId = scanner.nextLine();
+
+            Competitor competitor = competitorService.get(competitorId);
+
+            process.chooseContractor(competitor);
+
+            System.out.println("Concorrente escolhido com sucesso!");
+        } catch (Exception e) {
+            System.out.println("ERRO: " + e.getMessage());
+        }
+
     }
 }
