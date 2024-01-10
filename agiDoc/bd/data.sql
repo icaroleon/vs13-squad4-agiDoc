7fa90f3d46565759f4e22788835c1c10b4cb0fea
Gabriel Sales <gabriel.sales0440@gmail.com>
2024-01-10 17:39:07 -0300

    feat: bd script

---
+++ agiDoc/bd/data.sql
@@ -0,0 +1,29 @@
+INSERT INTO AGIDOC."USER" (id, email, password)
+VALUES (AGIDOC.SEQ_USER.NEXTVAL, 'icaro.leon@agidoc.com.br', '123');
+
+INSERT INTO AGIDOC."USER" (id, email, password)
+VALUES (AGIDOC.SEQ_USER.NEXTVAL, 'mariana.reis@agidoc.com.br', '123');
+
+INSERT INTO AGIDOC."USER" (id, email, password)
+VALUES (AGIDOC.SEQ_USER.NEXTVAL, 'rodrigo.costa@agidoc.com.br', '123');
+
+INSERT INTO AGIDOC."USER" (id, email, password)
+VALUES (AGIDOC.SEQ_USER.NEXTVAL, 'aron.adams@agidoc.com.br', '123');
+
+INSERT INTO AGIDOC."USER" (id, email, password)
+VALUES (AGIDOC.SEQ_USER.NEXTVAL, 'vinicius.barreto@agidoc.com.br', '123');
+
+INSERT INTO AGIDOC."USER" (id, email, password)
+VALUES (AGIDOC.SEQ_USER.NEXTVAL, 'clara.branco@agidoc.com.br', '123');
+
+INSERT INTO AGIDOC."USER" (id, email, password)
+VALUES (AGIDOC.SEQ_USER.NEXTVAL, 'camila.goncalves@agidoc.com.br', '123');
+
+INSERT INTO AGIDOC."USER" (id, email, password)
+VALUES (AGIDOC.SEQ_USER.NEXTVAL, 'gabriel.dantas@agidoc.com.br', '123');
+
+INSERT INTO AGIDOC."USER" (id, email, password)
+VALUES (AGIDOC.SEQ_USER.NEXTVAL, 'admin.admin@agidoc.com.br', '123');
+
+INSERT INTO AGIDOC."USER" (id, email, password)
+VALUES (AGIDOC.SEQ_USER.NEXTVAL, 'teste.teste@agidoc.com.br', '123');
