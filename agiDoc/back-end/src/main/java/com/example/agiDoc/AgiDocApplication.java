361b6027f2a43c26beb5c30dc9068cbfefdbd43d
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-01-23 12:41:39 -0300

    🎉 init: spring start

---
+++ agiDoc/back-end/src/main/java/com/example/agiDoc/AgiDocApplication.java
@@ -0,0 +1,13 @@
+package com.example.agiDoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgiDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgiDocApplication.class, args);
+	}
+
+}
