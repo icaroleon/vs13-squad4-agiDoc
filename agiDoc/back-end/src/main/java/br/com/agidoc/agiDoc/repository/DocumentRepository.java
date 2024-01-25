9b0aeb08a8b384f23577324a21f6e4350913932e
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-01-25 00:25:04 -0300

    ✨ feat: repositories

---
+++ agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/repository/DocumentRepository.java
@@ -0,0 +1,283 @@
+package br.com.agidoc.agiDoc.repository;
+
+import br.com.agidoc.agiDoc.database.DBConnection;
+import br.com.agidoc.agiDoc.exception.DatabaseException;
+import br.com.agidoc.agiDoc.model.Associated;
+import br.com.agidoc.agiDoc.model.document.Document;
+
+import java.sql.*;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class DocumentRepository implements IRepository<Integer, Document> {
+    public Integer getNextId(Connection con) throws SQLException {
+        String sql = "SELECT SEQ_DOCUMENTS.nextval mysequence from DUAL";
+
+        Statement stmt = con.createStatement();
+        ResultSet res = stmt.executeQuery(sql);
+
+        if (res.next()) {
+            return res.getInt("mysequence");
+        }
+
+        return null;
+    }
+
+    public Integer getAssociationNextId(Connection con) throws DatabaseException {
+        try {
+            String sql = "SELECT SEQ_DOCUMENTS_ASSOCIATIONS.NEXTVAL MY_SEQUENCE FROM DUAL";
+
+            Statement stmt = con.createStatement();
+            ResultSet res = stmt.executeQuery(sql);
+
+            if (res.next())
+                return res.getInt("MY_SEQUENCE");
+
+            return null;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        }
+    }
+
+    @Override
+    public Document create(Document document) throws DatabaseException {
+        Connection con = null;
+
+        try {
+            con = DBConnection.getConnection();
+
+            Integer nextId = this.getNextId(con);
+            Integer associationNextId = this.getAssociationNextId(con);
+            document.setId(nextId);
+
+            String sqlInsert = """
+                        INSERT INTO DOCUMENTS (ID_DOCUMENT, PROTOCOL, EXPIRATION_DATE, IS_SIGNED)
+                        VALUES (?, ?, ?, ?)
+                    """;
+
+            PreparedStatement stmt = con.prepareStatement(sqlInsert);
+
+            stmt.setInt(1, document.getId());
+            stmt.setString(2, document.getProtocol());
+            stmt.setString(3, document.getExpirationDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+            stmt.setInt(4, 0);
+
+            int res1 = stmt.executeUpdate();
+
+            System.out.println("createDocument.res=" + res1);
+
+            String sql2 = """
+                    INSERT INTO DOCUMENTS_ASSOCIATIONS (ID_DOCUMENT_ASSOCIATION, ID_DOCUMENT, ID_PROCESS)
+                    VALUES (?, ?, ?)
+                    """;
+
+            PreparedStatement stmt2 = con.prepareStatement(sql2);
+
+            stmt2.setInt(1, associationNextId);
+            stmt2.setInt(2, document.getId());
+            stmt2.setInt(3, document.getAssociatedId());
+
+            int res2 = stmt2.executeUpdate();
+            System.out.println("createAddressAssociation.res=" + res2);
+
+            return document;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public ArrayList<Document> list() throws DatabaseException {
+        ArrayList<Document> docs = new ArrayList<>();
+        Connection con = null;
+
+        try {
+            con = DBConnection.getConnection();
+            Statement stmt = con.createStatement();
+
+            String sqlSelect = "SELECT * FROM DOCUMENTS";
+
+            ResultSet res = stmt.executeQuery(sqlSelect);
+
+            while (res.next()) {
+                Document document = new Document();
+
+                document.setId(res.getInt("ID_DOCUMENT"));
+                document.setProtocol(res.getString("PROTOCOL"));
+                document.setExpirationDate(res.getDate("EXPIRATION_DATE").toLocalDate());
+
+                boolean isSigned = res.getInt("IS_SIGNED") == 1;
+                document.setSigned(isSigned);
+
+                String sql2 = "SELECT * FROM DOCUMENTS_ASSOCIATIONS WHERE ID_DOCUMENT = ?";
+
+                PreparedStatement stmt2 = con.prepareStatement(sql2);
+
+                stmt2.setInt(1, document.getId());
+
+                ResultSet res2 = stmt2.executeQuery();
+
+                if (res2.next()) {
+                    document.setAssociated(Associated.PROCESS);
+                    document.setAssociatedId(res2.getInt("ID_PROCESS"));
+                }
+
+                docs.add(document);
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return docs;
+    }
+
+    @Override
+    public boolean update(Integer id, Document document) throws DatabaseException {
+        Connection con = null;
+
+        try {
+            con = DBConnection.getConnection();
+
+            String sqlUpdate = "UPDATE DOCUMENTS SET PROTOCOL = ?, EXPIRATION_DATE = ? WHERE ID_DOCUMENT = ?";
+
+            PreparedStatement stmt = con.prepareStatement(sqlUpdate);
+
+            stmt.setString(1, document.getProtocol());
+            stmt.setString(2, document.getExpirationDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+            stmt.setInt(3, id);
+
+            int res = stmt.executeUpdate();
+            System.out.println("updateDocument.res=" + res);
+
+            return res > 0;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        } finally {
+            try {
+                if (con != null)
+                    con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public boolean delete(Integer id) throws DatabaseException {
+        Connection con = null;
+        try {
+            con = DBConnection.getConnection();
+
+            String sql1 = "DELETE FROM DOCUMENTS_ASSOCIATIONS WHERE ID_DOCUMENT = ?";
+
+            PreparedStatement stmt1 = con.prepareStatement(sql1);
+
+            stmt1.setInt(1, id);
+
+            int res1 = stmt1.executeUpdate();
+            System.out.println("deleteDocumentAssociation.res=" + res1);
+
+            String sql2 = "DELETE FROM DOCUMENTS WHERE ID_DOCUMENT = ?";
+
+            PreparedStatement stmt = con.prepareStatement(sql2);
+            stmt.setInt(1, id);
+
+            int res = stmt.executeUpdate();
+            System.out.println("deleteDocument.res=" + res);
+
+            return res > 0;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Document get(Integer id) throws DatabaseException {
+        Connection con = null;
+
+        try {
+            con = DBConnection.getConnection();
+
+            String sqlSelect = "SELECT * FROM DOCUMENTS WHERE ID_DOCUMENT = ?";
+
+            PreparedStatement stmt = con.prepareStatement(sqlSelect);
+            stmt.setInt(1, id);
+
+            ResultSet res = stmt.executeQuery();
+
+            Document document = new Document();
+
+            if (res.next()) {
+                document.setId(res.getInt("ID_DOCUMENT"));
+                document.setProtocol(res.getString("PROTOCOL"));
+                document.setExpirationDate(res.getDate("EXPIRATION_DATE").toLocalDate());
+
+                boolean isSigned = res.getInt("IS_SIGNED") == 1;
+                document.setSigned(isSigned);
+            }
+
+            return document;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        } finally {
+            try {
+                if (con != null)
+                    con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public boolean sign(Integer id, Integer signatureId) throws DatabaseException {
+        Connection con = null;
+
+        try {
+            con = DBConnection.getConnection();
+
+            String sqlUpdate = "UPDATE DOCUMENTS SET IS_SIGNED = ?, ID_SIGNATURE = ? WHERE ID_DOCUMENT = ?";
+
+            PreparedStatement stmt = con.prepareStatement(sqlUpdate);
+
+            stmt.setInt(1, 1);
+            stmt.setInt(2, signatureId);
+            stmt.setInt(3, id);
+
+            int res = stmt.executeUpdate();
+
+            return res > 0;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        } finally {
+            try {
+                if (con != null)
+                    con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/repository/UserRepository.java
@@ -0,0 +1,271 @@
+package br.com.agidoc.agiDoc.repository;
+
+import br.com.agidoc.agiDoc.database.DBConnection;
+import br.com.agidoc.agiDoc.exception.DatabaseException;
+import br.com.agidoc.agiDoc.model.department.Department;
+import br.com.agidoc.agiDoc.model.user.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class UserRepository implements IRepository<Integer, User> {
+    @Override
+    public Integer getNextId(Connection con) throws SQLException {
+        try {
+            String sql = "SELECT SEQ_USERS.nextval mysequence from DUAL";
+            Statement stmt = con.createStatement();
+            ResultSet res = stmt.executeQuery(sql);
+
+            if (res.next()) {
+                return res.getInt("mysequence");
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        }
+    }
+
+    @Override
+    public User create(User user) throws DatabaseException {
+        Connection con = null;
+        try {
+            if (user.getRegistration().isEmpty() || user.getName().isEmpty() || user.getUser().isEmpty() || user.getPassword().isEmpty() || user.getRole().isEmpty() || user.getPosition().isEmpty()) {
+                System.out.println("Nenhum campo pode estar em branco. Tente novamente.");
+                return null;
+            }
+            con = DBConnection.getConnection();
+            Integer nextId = this.getNextId(con);
+            user.setIdUser(nextId);
+
+            String sql = """
+                    INSERT INTO USERS
+                    (ID_USER, REGISTRATION, NAME, "USER", PASSWORD, "ROLE", "POSITION", ID_DEPARTMENT)
+                    VALUES(?,?,?,?,?,?,?,?)
+                    """;
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+            stmt.setInt(1, user.getIdUser());
+            stmt.setString(2, user.getRegistration());
+            stmt.setString(3, user.getName());
+            stmt.setString(4, user.getUser());
+            stmt.setString(5, user.getPassword());
+            stmt.setString(6, user.getRole());
+            stmt.setString(7, user.getPosition());
+            stmt.setInt(8, user.getDepartment().getIdDepartment());
+
+            int res = stmt.executeUpdate();
+            System.out.println("adicionarContato.res" + res);
+            return user;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public boolean update(Integer id, User user) throws DatabaseException {
+        Connection con = null;
+
+        if (id == null) {
+            System.out.println("Deve-se ser fornecido um id.");
+            return false;
+        }
+
+        try {
+            con = DBConnection.getConnection();
+            ArrayList<User> ls = listUser(id);
+
+            String checkSql = "SELECT COUNT(*) FROM USERS WHERE ID_USER = ?";
+            PreparedStatement checkStmt = con.prepareStatement(checkSql);
+            checkStmt.setInt(1, id);
+            ResultSet rs = checkStmt.executeQuery();
+            if (!rs.next() || rs.getInt(1) == 0) {
+                System.out.println("Nenhum usuário encontrado com o id fornecido.");
+                return false;
+            }
+
+            StringBuilder sql = new StringBuilder();
+            sql.append("UPDATE USERS SET ");
+            sql.append(" REGISTRATION = ?, ");
+            sql.append(" NAME = ?, ");
+            sql.append(" \"USER\" = ?, ");
+            sql.append(" PASSWORD = ?, ");
+            sql.append(" \"ROLE\" = ?, ");
+            sql.append(" \"POSITION\" = ?, ");
+            sql.append(" ID_DEPARTMENT = ? ");
+            sql.append(" WHERE ID_USER = ?");
+
+            PreparedStatement stmt = con.prepareStatement(sql.toString());
+
+            stmt.setString(1, user.getRegistration().isEmpty() ? ls.get(0).getRegistration() : user.getRegistration());
+            stmt.setString(2, user.getName().isEmpty() ? ls.get(0).getName() : user.getName());
+            stmt.setString(3, user.getUser().isEmpty() ? ls.get(0).getUser() : user.getUser());
+            stmt.setString(4, user.getPassword().isEmpty() ? ls.get(0).getPassword() : user.getPassword());
+            stmt.setString(5, user.getRole().isEmpty() ? ls.get(0).getRole() : user.getRole());
+            stmt.setString(6, user.getPosition().isEmpty() ? ls.get(0).getPosition() : user.getPosition());
+            stmt.setInt(7, user.getDepartment().getIdDepartment() == 0 ? ls.get(0).getDepartment().getIdDepartment() : user.getDepartment().getIdDepartment());
+            stmt.setInt(8, id);
+
+
+            int res = stmt.executeUpdate();
+            System.out.println("editarUser.res=" + res);
+
+            return res > 0;
+
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    @Override
+    public boolean delete(Integer id) throws DatabaseException {
+        Connection con = null;
+        if (id == null) {
+            System.out.println("Deve-se ser fornecido um id.");
+            return false;
+        }
+
+        try {
+            con = DBConnection.getConnection();
+
+            String checkSql = "SELECT COUNT(*) FROM USERS WHERE ID_USER = ?";
+            PreparedStatement checkStmt = con.prepareStatement(checkSql);
+            checkStmt.setInt(1, id);
+            ResultSet rs = checkStmt.executeQuery();
+            if (!rs.next() || rs.getInt(1) == 0) {
+                System.out.println("Nenhum usuário encontrado com o id fornecido.");
+                return false;
+            }
+
+            String sql = "DELETE FROM USERS WHERE ID_USER = ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, id);
+
+            int res = stmt.executeUpdate();
+            System.out.println("removerContatoPorId.res=" + res);
+
+            return res > 0;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        } catch (NumberFormatException e) {
+            System.out.println("O id fornecido não é um número.");
+            return false;
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public ArrayList<User> list() throws DatabaseException {
+        ArrayList<User> users = new ArrayList<>();
+        Connection con = null;
+        try {
+            con = DBConnection.getConnection();
+            Statement stmt = con.createStatement();
+
+            String sql = "SELECT U.*, D.NAME AS NAME_DEPARTMENT " +
+                    "FROM USERS U " +
+                    "LEFT JOIN DEPARTMENTS D ON (D.ID_DEPARTMENT = U.ID_DEPARTMENT)";
+
+
+            ResultSet res = stmt.executeQuery(sql);
+
+            while (res.next()) {
+                User user = getUserFromResultSet(res);
+                users.add(user);
+            }
+            return users;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public ArrayList<User> listUser (Integer id) throws  DatabaseException {
+        ArrayList<User> users = new ArrayList<>();
+        Connection con = null;
+
+        if (id == null) {
+            System.out.println("Deve-se ser fornecido um id.");
+            return null;
+        }
+        try {
+            con = DBConnection.getConnection();
+
+            String sql =  "SELECT U.*, D.NAME AS NAME_DEPARTMENT " +
+                    "FROM USERS U " +
+                    "INNER JOIN DEPARTMENTS D ON (D.ID_DEPARTMENT = U.ID_DEPARTMENT) " +
+                    "WHERE U.ID_USER = ?";
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet res = stmt.executeQuery();
+
+            while (res.next()) {
+                User user = getUserFromResultSet(res);
+                users.add(user);
+            }
+
+            if (users.isEmpty()) {
+                System.out.println("Usuário inexistente");
+            }
+
+            return users;
+        } catch (SQLException e) {
+            throw new DatabaseException(e.getCause());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private User getUserFromResultSet(ResultSet res) throws  SQLException {
+        User user = new User();
+        user.setIdUser(res.getInt("ID_USER"));
+        user.setName(res.getString("NAME"));
+        user.setUser(res.getString("USER"));
+        user.setPassword(res.getString("PASSWORD"));
+        user.setRegistration(res.getString("REGISTRATION"));
+        user.setPosition(res.getString("POSITION"));
+
+        Department department = new Department();
+        department.setName(res.getString("NAME_DEPARTMENT"));
+        department.setIdDepartment(res.getInt("ID_DEPARTMENT"));
+        user.setDepartment(department);
+        return user;
+    }
+}
