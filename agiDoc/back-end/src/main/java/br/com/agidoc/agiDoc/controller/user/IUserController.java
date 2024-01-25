44ffc51bd67f015e35c47b660cff615145b6f7c7
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-01-25 15:33:51 -0300

    ✨ feat: User start

---
+++ agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/controller/user/IUserController.java
@@ -0,0 +1,62 @@
+package br.com.agidoc.agiDoc.controller.user;
+
+import br.com.agidoc.agiDoc.exception.DatabaseException;
+import br.com.agidoc.agiDoc.model.user.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+import java.util.List;
+
+public interface IUserController {
+    @Operation(summary = "Create User", description = "Create a user in database")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Return created user."),
+                    @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true)), description = "Wrong data inserted, check the error in response."),
+                    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(hidden = true)), description = "Unhandled exception.")
+            }
+    )
+    User create(User user) throws Exception;
+
+    @Operation(summary = "List Users", description = "List all users in database")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Return a list of users."),
+                    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(hidden = true)), description = "Unhandled exception.")
+            }
+    )
+    List<User> list() throws Exception;
+
+    @Operation(summary = "List Users by id", description = "List users by id in database")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Return a list of users with a given id."),
+                    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(hidden = true)), description = "Unhandled exception.")
+            }
+    )
+    List<User> listById(Integer id) throws Exception;
+
+    @Operation(summary = "Update User", description = "Update a user in database")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Return updated user."),
+                    @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true)), description = "User not found."),
+                    @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true)), description = "Wrong data inserted, check the error in response."),
+                    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(hidden = true)), description = "Unhandled exception.")
+            }
+    )
+    boolean update(Integer id, User user) throws Exception;
+
+    @Operation(summary = "Delete User", description = "Delete a user in database")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "User deleted successfully."),
+                    @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true)), description = "User not found."),
+                    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(hidden = true)), description = "Unhandled exception.")
+            }
+    )
+    boolean delete(Integer id) throws Exception;
+}
---
+++ agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/controller/user/UserController.java
@@ -0,0 +1,45 @@
+package br.com.agidoc.agiDoc.controller.user;
+
+import br.com.agidoc.agiDoc.exception.DatabaseException;
+import br.com.agidoc.agiDoc.model.user.User;
+import br.com.agidoc.agiDoc.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Tag(name = "User", description = "CRUD of users")
+@RestController
+@Validated
+@RequestMapping("/user")
+public class UserController implements IUserController{
+    private final UserService userService;
+
+    @PostMapping
+    public User create(@RequestBody User user) throws Exception {
+        return this.userService.create(user);
+    }
+
+    @GetMapping
+    public List<User> list() throws Exception {
+        return this.userService.list();
+    }
+
+    @GetMapping("/{id}")
+    public List<User> listById(@PathVariable Integer id) throws Exception {
+        return this.userService.listById(id);
+    }
+
+    @PutMapping("/{id}")
+    public boolean update(@PathVariable Integer id,@RequestBody User user) throws Exception {
+        return this.userService.update(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable Integer id) throws Exception {
+        return this.userService.delete(id);
+    }
+}
