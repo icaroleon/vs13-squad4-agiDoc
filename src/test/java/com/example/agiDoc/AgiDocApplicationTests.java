69be38aac0f043ff362304e30dd95bfd74523925
ViniPiresB <viniciuspbarreto1@gmail.com>
2024-01-28 12:09:13 -0300

    🐎 ci: comment test file

--- src/test/java/com/example/agiDoc/AgiDocApplicationTests.java
+++ src/test/java/com/example/agiDoc/AgiDocApplicationTests.java
@@ -1,13 +1,15 @@
-package com.example.agiDoc;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AgiDocApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.example.agiDoc;
+ * 
+ * import org.junit.jupiter.api.Test;
+ * import org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest
+ * class AgiDocApplicationTests {
+ * 
+ * @Test
+ * void contextLoads() {
+ * }
+ * 
+ * }
+ */
