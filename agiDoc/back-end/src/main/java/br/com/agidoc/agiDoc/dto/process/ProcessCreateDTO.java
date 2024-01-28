ff3f8567b8955750fd762f2d5120b5b93100be0b
Ícaro Leon <icvieiramg@gmail.com>
2024-01-28 17:56:31 -0300

    start edit DTO

--- agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/dto/process/ProcessCreateDTO.java
+++ agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/dto/process/ProcessCreateDTO.java
@@ -1,19 +1,10 @@
 package br.com.agidoc.agiDoc.dto.process;
 
-
-import br.com.agidoc.agiDoc.model.competitor.Competitor;
-import br.com.agidoc.agiDoc.model.document.Document;
-import br.com.agidoc.agiDoc.model.process.ProcessStatus;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
-import java.util.UUID;
-
 @NoArgsConstructor
 @Data
 public class ProcessCreateDTO {
