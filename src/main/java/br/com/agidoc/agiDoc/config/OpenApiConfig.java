2ed4764d62509b11f60831cc762d80e22ad2115b
ViniPiresB <viniciuspbarreto1@gmail.com>
2024-01-28 16:16:52 -0300

    feat: OpenApiConfig

---
+++ src/main/java/br/com/agidoc/agiDoc/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package br.com.agidoc.agiDoc.config;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info().title("AgiDoc API")
+                        .description("AgiDoc API documentação")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
+    }
+}
