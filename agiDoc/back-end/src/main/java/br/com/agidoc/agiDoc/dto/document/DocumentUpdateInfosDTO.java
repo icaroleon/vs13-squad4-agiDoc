38efc92a6f233ee84e07695468f3318f274ea818
Ícaro Leon <icvieiramg@gmail.com>
2024-01-29 10:44:06 -0300

    edit infos dto

--- agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/dto/document/DocumentUpdateInfosDTO.java
+++ agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/dto/document/DocumentUpdateInfosDTO.java
@@ -1,14 +1,26 @@
 package br.com.agidoc.agiDoc.dto.document;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.time.LocalDate;
 
 public class DocumentUpdateInfosDTO {
 
-    @NotBlank(message = "Title must not be blank")
+    @Schema(example = "Edital de Abertura de Processo Licitatório")
     private String title;
 
-    @NotBlank(message = "Description must not be blank")
     @Size(max = 500, message = "Description must not be longer than 500 characters")
-    private String descriptionProcess;
+    private String description;
+
+    @Schema(description = "Path or name of the file", example = "document.pdf")
+    private String file;
+
+    @FutureOrPresent(message = "Expiration date must be in the future or present")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    @Schema(example = "30-12-2024")
+    private LocalDate expirationDate;
 }
