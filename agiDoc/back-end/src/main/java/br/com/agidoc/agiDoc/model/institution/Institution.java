601f40f7f91d081230e4d75fcf702214082a7d28
Carlos-Academico <henrique.queiroz@academico.ifpb.edu.br>
2024-01-29 09:45:02 -0300

    fix: adicionando atributos a institution.

--- agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/model/institution/Institution.java
+++ agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/model/institution/Institution.java
@@ -4,59 +4,25 @@ import br.com.agidoc.agiDoc.model.process.Process;
 import br.com.agidoc.agiDoc.model.juridical.AbstractJuridical;
 import br.com.agidoc.agiDoc.model.address.Address;
 import br.com.agidoc.agiDoc.model.contact.Contact;
+import br.com.agidoc.agiDoc.model.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 import java.util.ArrayList;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Institution extends AbstractJuridical {
 
     private Integer id;
+    private String companyName;
+    private String cnpj;
+    private Address address;
+    private Contact contact;
+    private ArrayList<User> users;
     private ArrayList<Process> processes;
 
-
-    public Institution() {
-        super();
-    }
-
-    public Institution(String companyName, String cnpj, Address address, Contact contact, ArrayList<Process> processes) {
-        super(companyName, cnpj, address, contact);
-        this.processes = processes;
-    }
-
-    public Institution(Integer idInstitution, String companyName,String cnpj){
-        setId(idInstitution);
-        setCompanyName(companyName);
-        setCnpj(cnpj);
-    }
-
-    @Override
-    public String toString() {
-        return """ 
-                {
-                    CNPJ: %s
-                    Company Name: %s
-                    Address: %s
-                    Contact: %s
-                }
-                """.formatted(
-                this.cnpj,
-                this.companyName,
-                this.address,
-                this.contact
-        ).replace("Null", "não informado.");
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public ArrayList<Process> getProcesses() { return this.processes; }
-
-    public void setProcesses(ArrayList<Process> processes) {
-        this.processes = processes;
-    }
 }
