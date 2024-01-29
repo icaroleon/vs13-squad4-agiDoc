e6bf95f8a2600ebfc5e9500f57419ce7b4078006
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2024-01-29 00:29:14 -0300

    ✨ feat: Competitor DTOs

--- agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/dto/juridical/JuridicalDTO.java
+++ agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/dto/juridical/JuridicalDTO.java
@@ -1,8 +1,7 @@
-package br.com.agidoc.agiDoc.model.juridical;
+package br.com.agidoc.agiDoc.dto.juridical;
 
 import br.com.agidoc.agiDoc.model.address.Address;
 import br.com.agidoc.agiDoc.model.contact.Contact;
-import br.com.agidoc.agiDoc.model.document.Document;
 import br.com.agidoc.agiDoc.model.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,22 +12,10 @@ import java.util.ArrayList;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public abstract class AbstractJuridical implements IJuridical {
+public class JuridicalDTO {
     protected String cnpj;
     protected String companyName;
     protected Address address;
     protected Contact contact;
     protected ArrayList<User> users;
-
-    @Override
-    public boolean signDocument(Document document) {
-        System.out.println("Documento assinado!");
-        return true;
-    }
-
-    @Override
-    public boolean sendSignedDocument(Process process) {
-        System.out.println("Documento enviado com sucesso!");
-        return false;
-    }
 }
