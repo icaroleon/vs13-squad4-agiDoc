b2a79cf0acf93b5ff71dafd629552e8e82a00eea
Ícaro Leon <icvieiramg@gmail.com>
2024-02-19 18:05:18 -0300

    mongoDB ok

---
+++ agiDoc/task/task01-findByStatusOperation.js
@@ -0,0 +1,29 @@
+db.processes.find({"status": /IN_PROGRESS/})
+{
+  _id: ObjectId('65d3747beb0ec33e844466af'),
+  processNumber: UUID('509a42bd-2cb5-4ca1-8959-c6b27975b258'),
+  title: 'Nova Licitação',
+  description: 'Construção de uma ponte na cidade de São Paulo',
+  status: 'IN_PROGRESS'
+}
+{
+  _id: ObjectId('65d3747beb0ec33e844466b0'),
+  processNumber: UUID('c04a9a28-8c08-4a08-80b2-f00666706236'),
+  title: 'Licitação de Obra Pública',
+  description: 'Pavimentação da Estrada X na Cidade de Porto Alegre',
+  status: 'IN_PROGRESS'
+}
+{
+  _id: ObjectId('65d3747beb0ec33e844466b1'),
+  processNumber: UUID('2a8a90c2-1c3e-4582-ba78-265df7d403bb'),
+  title: 'Nova Licitação',
+  description: 'Construção de uma ponte na cidade de São Paulo',
+  status: 'IN_PROGRESS'
+}
+{
+  _id: ObjectId('65d3747beb0ec33e844466b2'),
+  processNumber: UUID('83e44640-4712-4795-ac29-7d9f8723675a'),
+  title: 'Processo de Compra',
+  description: 'Aquisição de materiais de escritório',
+  status: 'IN_PROGRESS'
+}
