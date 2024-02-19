7311e5b618010e5747ca590e9283d64885c3ccb3
Mariana Nascimento <mariana.sn.silva@gmail.com>
2024-02-19 19:02:46 -0300

    add: mongoDB

---
+++ agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/dto/log/LogCreateDTO.java
@@ -0,0 +1,16 @@
+package br.com.agidoc.agiDoc.dto.log;
+
+import br.com.agidoc.agiDoc.model.log.LogType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LogCreateDTO {
+    private String id;
+    private LogType logType;
+    private String description;
+    private String date;
+}
---
+++ agiDoc/back-end/src/main/java/br/com/agidoc/agiDoc/model/log/LogType.java
@@ -0,0 +1,9 @@
+package br.com.agidoc.agiDoc.model.log;
+
+public enum LogType {
+    ADMIN_COMPETITOR,
+    USER_COMPETITOR,
+    ADMIN_INSTITUTION,
+    USER_INSTITUTION,
+    SYSTEM_ADMIN
+}
